e1d34c9e49d6e06e9a2f6fcef851c5f83f66788b
Isreal6ix <isrealsix@gmail.com>
2021-09-05 23:44:08 +0700

    styled the header component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCog, faToolbox } from '@fortawesome/free-solid-svg-icons';
 
 const CarItem = () => {
 	return (
 		<View style={styles.carContainer}>
 			{/* Header */}
 			<View style={styles.header}>
-				<Text>Settings</Text>
-				<Text>My Mobile</Text>
-				<Text>Surprise</Text>
+				<FontAwesomeIcon icon={faCog} style={styles.icon} size={24} />
+				<Text style={styles.headerTitle}>My Mobile</Text>
+				<FontAwesomeIcon icon={faToolbox} style={styles.icon} size={24} />
 			</View>
 		</View>
 	);
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -4,10 +4,25 @@ const styles = StyleSheet.create({
 	carContainer: {
 		width: '100%',
 		height: '100%',
-		backgroundColor: 'red',
+		backgroundColor: '#ddd',
 	},
 	header: {
 		marginTop: 50,
+		marginLeft: 20,
+		marginRight: 20,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	headerTitle: {
+		color: '#fff',
+		fontSize: 16,
+		fontWeight: 'bold',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	icon: {
+		color: '#fff',
 	},
 });
 
