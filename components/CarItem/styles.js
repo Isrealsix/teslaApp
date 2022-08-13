5e06bcf2468220c16480825eb2ab332e02fb59bb
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-08-14 01:21:41 +0700

    added background color

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,30 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-	carContainer: {
-		width: '100%',
-		height: '100%',
-		// backgroundColor: '#ddd',
-	},
-	header: {
-		marginTop: 50,
-		marginLeft: 20,
-		marginRight: 20,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
-	headerTitle: {
-		color: '#fff',
-		fontSize: 16,
-		fontWeight: 'bold',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	icon: {
-		color: '#fff',
-		fontSize: '26px',
-	},
+  carContainer: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#ddd",
+  },
+  header: {
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: {
+    color: "#fff",
+    fontSize: "26px",
+  },
 });
 
 export default styles;
